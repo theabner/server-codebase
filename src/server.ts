d990d093b15245364ab4d72c62adc6e0a8e706d8
Abner Matheus <abner.silva@climatempo.com.br>
2023-07-26 12:37:19 -0300

    feat: add security packages for server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,29 @@
-import { createHandler } from 'graphql-http/lib/use/express'
+import 'dotenv/config'
 import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import cors from 'cors'
+import rateLimit from 'express-rate-limit'
+import { createHandler } from 'graphql-http/lib/use/express'
 import { routes } from './routes'
 import { schema } from './schema'
 
+
 const server = express()
 
+server.use(cors({
+  origin: `http://localhost:${process.env.SERVER_PORT}`,
+}))
+
+server.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true,
+  legacyHeaders: false,
+}))
+
+server.use(helmet())
+server.use(morgan('dev'))
 server.use(express.json())
 
 server.use(routes)
