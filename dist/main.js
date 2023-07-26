08d06ea29d86676339f6b5cad0209df43ddcab45
Abner Matheus <theabnermatheus@gmail.com>
2023-07-26 01:00:48 -0300

    feat: delete dist and prisma migrations pasta

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-const server_1 = require("./server");
-server_1.server.listen(3000, () => {
-    console.log('Listening on port 3000');
-});
