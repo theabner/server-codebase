6cb04690219cbbe8a53abf86216db122b1eba350
Abner Matheus <abner.silva@climatempo.com.br>
2023-08-18 10:44:54 -0300

    feat: add chalk message

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import 'reflect-metadata'
 import 'dotenv/config'
+import chalk from 'chalk'
 import { Server } from './server'
 
 const port = process.env.SERVER_PORT
+const isProd = process.env.NODE_ENV === 'PROD'
+
+if (isProd)
+  console.log(
+    chalk.red(`
+    Caution: This server is running in production mode. 
+    Be cautious with your actions as they will directly impact the live environment. 
+    Double-check before making critical changes and ensure you have necessary backups.
+  `),
+  )
+else
+  console.log(
+    chalk.yellow(`
+  Heads up: This server is currently running in development mode. 
+  Feel free to experiment and make changes without affecting the live environment. 
+  This is your playground to innovate and perfect.
+  `),
+  )
 
 Server.listen({ port: Number(port) }, (err, address) => {
   if (err) {
